fb8f9c9bb65303813f3463b5901ac10a9d5b9eb7
SAlexeyG <56839962+SAlexeyG@users.noreply.github.com>
2020-11-29 23:27:30 +0300

    no message

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino
+{
+	private Transform transform;
+	private TetrominoItem[] tetrominoItems;
+	private IInput inputManager;
+
+	Tetromino(Transform _transform, IInput _input, TetrominoVariant variant)
+	{
+		transform = _transform;
+		inputManager = _input;
+		tetrominoItems = new TetrominoItem[variant.tetrominoItemPositions.Length];
+
+		for(int i = 0; i < tetrominoItems.Length; i++)
+		{
+			tetrominoItems[i] = new TetrominoItem();
+			tetrominoItems[i].spriteRenderer.sprite = variant.sprite;
+			tetrominoItems[i].transform = new Transform();
+		}
+	}
+}
