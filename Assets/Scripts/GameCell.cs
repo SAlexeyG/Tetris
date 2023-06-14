318a0d373c29f8a60c54888debf37c90472a8520
SAlexeyG <56839962+SAlexeyG@users.noreply.github.com>
2023-06-14 14:09:55 +0300

    GameCells added

---
+++ Assets/Scripts/GameCell.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	internal class GameCell : ICell
+	{
+		private const int contactsCapacity = 8;
+		private Transform transform;
+		private Collider2D collider;
+		private Collider2D[] contacts;
+
+		public Vector3 Position { get => transform.position; set => throw new NotImplementedException(); }
+		public Quaternion Rotation { get => transform.rotation; set => throw new NotImplementedException(); }
+		public Vector3 Scale { get => transform.lossyScale; set => throw new NotImplementedException(); }
+		public ICollection<Vector3> Collisions 
+		{
+			get
+			{
+				collider.GetContacts(contacts);
+				return contacts.Select(contact => contact.gameObject.transform.position).ToList();
+			}
+			
+			set => throw new NotImplementedException(); 
+		}
+
+		public GameCell(GameObject prefab, Vector3 position)
+		{
+			contacts = new Collider2D[contactsCapacity];
+			var cell = GameObject.Instantiate(prefab, position, Quaternion.identity);
+			transform = cell.transform;
+			collider = cell.transform.GetComponent<Collider2D>();
+		}
+	}
+}
---
+++ Assets/Scripts/ICell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	internal interface ICell
+	{
+		Vector3 Position { get; set; }
+		Quaternion Rotation { get; set; }
+		Vector3 Scale { get; set; }
+		ICollection<Vector3> Collisions { get; set;}
+	}
+}
